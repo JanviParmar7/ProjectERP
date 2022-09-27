8c7385b7d5880cce889e1daecf5a7b276cd3468b
Janvi Parmar <janvi7parmar2000@gmail.com>
2022-09-27 17:16:59 +0530

    Adding custom field in custom field and some changing regarding to custom field in hook.py

--- local1/www/userinform.py
+++ local1/www/userinform.py
@@ -1,10 +1,6 @@
 from dataclasses import fields
 import frappe
 
-
-# def get_context(context):
-#     context.users = frappe.get_list("Registration", fields=["firstname","lastname","fullname","dateofbirth","mobilenumber","gender","city","state","email","address"])
-
 listuser = []
 def get_context(context):
         for info in listuser:
@@ -16,6 +12,5 @@ def get_context(context):
 def userData(data):
         global all
         data = frappe.db.get_list("Registration",filters={'email':data},fields=["photo","firstname","lastname","fullname","dateofbirth","mobilenumber","gender","city","state","email","address","signature"])
-  
         listuser.append(data)
         
